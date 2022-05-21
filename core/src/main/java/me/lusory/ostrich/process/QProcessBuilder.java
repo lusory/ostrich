8eb2e10809f1cdede44af405b63f9afd3d2399e1
zlataovce <34477304+zlataovce@users.noreply.github.com>
2022-05-21 14:56:44 +0200

    chore: delombok MiscUtils, javadocs

--- core/src/main/java/me/lusory/ostrich/process/QProcessBuilder.java
+++ core/src/main/java/me/lusory/ostrich/process/QProcessBuilder.java
@@ -4,10 +4,30 @@ import java.io.File;
 import java.io.IOException;
 
 public interface QProcessBuilder {
+    /**
+     * Assembles the process arguments based on the builder contents.
+     *
+     * @return the arguments, not including the executable
+     */
     String[] getArguments();
 
+    /**
+     * Launches the supplied executable with the arguments from {@link #getArguments()}.
+     *
+     * @param executable the executable
+     * @return the process
+     * @throws IOException when a process I/O error occurs
+     */
     Process build(File executable) throws IOException;
 
+    /**
+     * Launches the supplied executable with the arguments from {@link #getArguments()} and waits until the process exits.
+     *
+     * @param executable the executable
+     * @return the process return code
+     * @throws IOException when a process I/O error occurs
+     * @throws InterruptedException when the current thread is interrupted by another thread while it is waiting
+     */
     default int buildAndBlock(File executable) throws IOException, InterruptedException {
         return build(executable).waitFor();
     }
--- core/src/main/java/me/lusory/ostrich/process/util/MiscUtils.java
+++ core/src/main/java/me/lusory/ostrich/process/util/MiscUtils.java
@@ -1,12 +1,13 @@
 package me.lusory.ostrich.process.util;
 
-import lombok.experimental.UtilityClass;
-
 import java.util.Arrays;
 
-@UtilityClass
-public class MiscUtils {
-    public <T> T[] concat(T[] first, T[] second) {
+public final class MiscUtils {
+    private MiscUtils() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static <T> T[] concat(T[] first, T[] second) {
         T[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
